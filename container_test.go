fee699fb7d48f7fc51343198dc137ff297f9d8d5
myOmikron <git@omikron.dev>
2022-03-25 09:26:41 +0100

    Added filter for slices

--- container_test.go
+++ container_test.go
@@ -2,9 +2,11 @@ package masochism_test
 
 import (
 	"github.com/myOmikron/masochism"
+	"strings"
 	"testing"
 )
 
+//TestForeach This function will test slices
 func TestForeach(t *testing.T) {
 	a := []int{1, 2, 3, 4, 5}
 
@@ -19,5 +21,18 @@ func TestForeach(t *testing.T) {
 		if a[i] != b[i] {
 			t.Error("elements order missmatch")
 		}
+	}
+}
+
+func TestFilter(t *testing.T) {
+	s := []string{"abc", "def", "fgh"}
+	a := masochism.Filter(func(t string) bool { return strings.HasPrefix(t, "d") })(s)
+
+	if len(a) != 1 {
+		t.Error("length mismatch")
+	}
+
+	if a[0] != "def" {
+		t.Error("Wrong element in slice")
 	}
 }
